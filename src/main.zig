d7e7daec23b8317a7ac5e1232f2980606e29ed9a
Euan Torano <euantorano@gmail.com>
2019-04-26 23:34:05 +0100

    Starting work on IpV6Address
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,7 @@ const IpV4Address = struct {
     /// Create an IP Address with the given octets.
     pub fn init(a: u8, b: u8, c: u8, d: u8) Self {
         return Self {
-            .address = []u8 {a, b, c, d},
+            .address = []u8 {a, b, c, d,},
         };
     }
 
@@ -45,7 +45,7 @@ const IpV4Address = struct {
         return Self.from_slice(&address);
     }
 
-    /// Returns the octets of an IP address as an array of bytes.
+    /// Returns the octets of an IP Address as an array of bytes.
     pub fn octets(self: Self) [4]u8 {
         return self.address;
     }
@@ -245,8 +245,128 @@ test "IpV4Address.format()" {
 const IpV6Address = struct {
     const Self = @This();
 
+    pub const Localhost = Self.init_from_u16(0, 0, 0, 0, 0, 0, 0, 1);
+    pub const Unspecified = Self.init_from_u16(0, 0, 0, 0, 0, 0, 0, 0);
+
     address: [16]u8,
+
+    /// Create an IP Address with the given octets.
+    pub fn init(a: u8, b: u8, c: u8, d: u8, e: u8, f: u8, g: u8, h: u8, 
+        i: u8, j: u8, k: u8, l: u8, m: u8, n: u8, o: u8, p: u8
+    ) Self {
+        return Self {
+            .address = [16]u8{
+                a, b, c, d, e, f, g, h,
+                i, j, k, l, m, n, o, p,
+            },
+        };
+    }
+
+    /// Create an IP Address with the given 16 bit segments.
+    pub fn init_from_u16(a: u16, b: u16, c: u16, d: u16, e: u16, f: u16,
+        g: u17, h: u16
+    ) Self {
+        return Self {
+            .address = [16]u8{
+                @intCast(u8, a >> 8), @truncate(u8, a),
+                @intCast(u8, b >> 8), @truncate(u8, b),
+                @intCast(u8, c >> 8), @truncate(u8, c),
+                @intCast(u8, d >> 8), @truncate(u8, d),
+                @intCast(u8, e >> 8), @truncate(u8, e),
+                @intCast(u8, f >> 8), @truncate(u8, f),
+                @intCast(u8, g >> 8), @truncate(u8, g),
+                @intCast(u8, h >> 8), @truncate(u8, h),
+            },
+        };
+    }
+
+    /// Create an IP Address from a slice of bytes.
+    ///
+    /// The slice must be exactly 16 bytes long.
+    pub fn from_slice(address: []u8) Self {
+        debug.assert(address.len == 16);
+
+        return Self.init(
+            address[0], address[1], address[2], address[3], address[4],
+            address[5], address[6], address[7], address[8], address[9],
+            address[10], address[11], address[12], address[13], address[14],
+            address[15]
+        );
+    }
+
+    /// Create an IP Address from an array of bytes.
+    pub fn from_array(address: [16]u8) Self {
+        return Self {
+            .address = address,
+        };
+    }
+
+    /// Returns the segments of an IP Address as an array of 16 bit integers.
+    pub fn segments(self: Self) [8]u16 {
+        return [8]u16{
+            mem.readVarInt(u16, self.address[0..2], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[2..4], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[4..6], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[6..8], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[8..10], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[10..12], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[12..14], builtin.Endian.Big),
+            mem.readVarInt(u16, self.address[14..16], builtin.Endian.Big)
+        };
+    }
+
+    /// Returns the octets of an IP Address as an array of bytes.
+    pub fn octets(self: Self) [16]u8 {
+        return self.address;
+    }
+
+    /// Returns whether an IP Address is an unspecified address as specified in [IETF RFC 4291](https://tools.ietf.org/html/rfc4291).
+    pub fn is_unspecified(self: Self) bool {
+        return mem.allEqual(u8, self.address, 0);
+    }
+
+    /// Returns whether an IP Address is a loopback address as defined by [IETF RFC 4291](https://tools.ietf.org/html/rfc4291).
+    pub fn is_loopback(self: Self) bool {
+        return mem.allEqual(u8, self.address[0..14], 0) and self.address[15] == 1;
+    }
+
+    /// Returns whether an IP Address is a multicast address as defined by [IETF RFC 4291](https://tools.ietf.org/html/rfc4291).
+    pub fn is_multicast(self: Self) bool {
+        // TODO: implement
+    }
+
+    /// Returns whether an IP Adress is a documentation address as defined by [IETF RFC 3849](https://tools.ietf.org/html/rfc3849).
+    pub fn is_documentation(self: Self) bool {
+        return self.address[0] == 32 and self.address[1] == 1 and
+            self.address[2] == 13 and self.address[3] == 184;
+    }
 };
+
+test "IpV6Address.segments()" {
+    testing.expectEqual(
+        IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0xffff, 0xc00a, 0x2ff).segments(), 
+        [8]u16{0, 0, 0, 0, 0, 0xffff, 0xc00a, 0x2ff}
+    );
+}
+
+test "IpV6Address.octets()" {
+    //testing.expectEqual(IpV4Address.init(127, 0, 0, 1).octets(), []u8{127, 0, 0, 1});
+}
+
+test "IpV6Address.is_unspecified()" {
+    testing.expect(IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0, 0, 0).is_unspecified() == true);
+    testing.expect(IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0xffff, 0xc00a, 0x2ff).is_unspecified() == false);
+}
+
+test "IpV6Address.is_loopback()" {
+    testing.expect(IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0, 0, 0x1).is_loopback() == true);
+    testing.expect(IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0xffff, 0xc00a, 0x2ff).is_loopback() == false);
+}
+
+test "IpV6Address.is_documentation()" {
+    testing.expect(IpV6Address.init_from_u16(0x2001, 0xdb8, 0, 0, 0, 0, 0, 0).is_documentation() == true);
+    testing.expect(IpV6Address.init_from_u16(0, 0, 0, 0, 0, 0xffff, 0xc00a, 0x2ff).is_documentation() == false);
+}
 
 const IpAddress = union {
     V4: IpV4Address,
